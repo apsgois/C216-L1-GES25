5160cd9f5533435f057765b66609b4f0c254c68f
apsgois <apsgois@outlook.com>
2024-03-08 14:43:07 -0300

    Inicio

---
+++ pratica-0/src/index.js
@@ -0,0 +1,9 @@
+
+let nome = "Ana Paula Serafim de Gois - 25";
+function saudacaoPersonalizada(nome) {
+    console.log("Olá, " + nome + "!");
+}
+saudacaoPersonalizada(nome);
+
+let modulo = require('./module.js');
+modulo.saudacao();
---
+++ pratica-0/src/module.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mensagem: 'Testando modulo: Ana Paula - 25 !',
+    saudacao: function() {
+      console.log(this.mensagem);
+    }
+  };
